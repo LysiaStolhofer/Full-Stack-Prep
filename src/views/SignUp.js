1d025bdf71bac62dcf46e0cd11f2bac7ecba3265
Lysia Stolhofer <lysiapaulinstolhofer@gmail.com>
2020-01-21 20:35:53 -0500

    FIles modified / Routers deleted

--- src/views/SignUp.js
+++ src/views/SignUp.js
@@ -1,27 +1,20 @@
 import React from "react";
-// import { Container, Row, Col } from "reactstrap";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Home from "./Home";
+import { BrowserRouter as Route, Link } from "react-router-dom";
 import Landing from "../components/Landing";
 
 const SignUp = () => {
   return (
-    <Router>
+    <React.Fragment>
       <h2>THIS IS THE SIGN UP PAGE</h2>
-
       <div>
-        
         <ul>
           <li>
             <Link to="/">Done - back to Splash-page</Link>
           </li>
         </ul>
-
-        <hr />
-
         <Route exact path="/" component={Landing} />
       </div>
-    </Router>
+    </React.Fragment>
   );
 };
 
