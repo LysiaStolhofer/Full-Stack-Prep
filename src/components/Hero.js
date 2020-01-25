74e2be603b78341231291107da5a82af29fc5b3c
bcwahoo <bcwahoo@gmail.com>
2020-01-25 13:43:54 -0500

    Last Changes of the day

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,9 @@
 import React from "react";
-// import logo from "../assets/logo.svg";
+import logo from "../assets/images/logo.svg";
 
 const Hero = () => (
   <div className="text-center hero my-5">
-    {/* <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" /> */}
+    <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
     <h1 className="mb-4">Full Stack Prep</h1>
   </div>
 );
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,4 +1,4 @@
-import React from 'react'; 
-const Landing = () => <div></div>
+import React from 'react';
+const Landing = () => <div>BLAH BLAH BLAH </div>
 
 export default Landing; 
