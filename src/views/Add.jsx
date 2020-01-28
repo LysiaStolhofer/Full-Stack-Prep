a0781122b50ae1345e2cd78d7aa09a968cdd594b
bcwahoo <bcwahoo@gmail.com>
2020-01-27 21:42:54 -0500

    Meeting Upate

--- src/views/Add.jsx
+++ src/views/Add.jsx
@@ -1,83 +1,68 @@
 import React from "react";
-import { Form, Card } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
 import { Col, Button } from "reactstrap";
 import { Link } from "react-router-dom";
-
+import './Add.css'
 const Add = () => {
   return (
     <React.Fragment>
-      <h3 className="text-center">Add an Interview Question</h3>
-
+      <h2 id="font">Add an interview question</h2>
       <hr />
-      <Card>
-        <Card.Body>
-          <Form>
-            <Form.Row>
-              <Form.Group as={Col} controlId="formInputTopic">
-                <Form.Label>Select Full Stack Topic</Form.Label>
-                <Form.Control as="select">
-                  <option>Frontend Fundamentals</option>
-                  <option>Javascript Fundamentals</option>
-                  <option>Repository Fundamentals</option>
-                  <option>Database Fundamentals</option>
-                  <option>JS Server Fundamentals</option>
-                  <option>Framework Fundamentals</option>
-                  <option>Mixed Topics</option>
-                </Form.Control>
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formInputLevel">
-                <Form.Label>Select Question Difficulty</Form.Label>
-                <Form.Control as="select">
-                  <option>Entry</option>
-                  <option>Junior</option>
-                  <option>Mid-Level</option>
-                </Form.Control>
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formInputTimer">
-                <Form.Label>Time to Answer</Form.Label>
-                <Form.Control as="select">
-                  <option>1 minute</option>
-                  <option>2 minute</option>
-                  <option>3 minute</option>
-                  <option>4 minute</option>
-                  <option>5 minute</option>
-                </Form.Control>
-              </Form.Group>
-            </Form.Row>
-
-            <Form.Group controlId="formInputQuestion">
-              <Form.Label>Add the Question You want to add to Full Stack Prep</Form.Label>
-              <Form.Control as="textarea" rows="4" />
-            </Form.Group>
-
-            <Form.Group controlId="formInputAnswer">
-              <Form.Label>What is the Answer to the Question.</Form.Label>
-              <Form.Control as="textarea" rows="3" />
-            </Form.Group>
-
-            <Form.Group controlId="formInputCode">
-              <Form.Label>If your Question has any code, Enter it here (Seperate it by Enter Key)</Form.Label>
-              <Form.Control as="textarea" rows="3" />
-            </Form.Group>
-
-            <Form.Group id="formGridCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-
-            <Button variant="primary" type="submit">
-              Submit
+      <Form id="font">
+        <Form.Row>
+          <Form.Group as={Col} controlId="formInputTopic">
+            <Form.Label>Select Full Stack Topic</Form.Label>
+            <Form.Control as="select">
+              <option>Frontend Fundamentals</option>
+              <option>Javascript Fundamentals</option>
+              <option>Repository Fundamentals</option>
+              <option>Database Fundamentals</option>
+              <option>JS Server Fundamentals</option>
+              <option>Framework Fundamentals</option>
+              <option>Mixed Topics</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group as={Col} controlId="formInputLevel">
+            <Form.Label>Select Question Difficulty</Form.Label>
+            <Form.Control as="select">
+              <option id="yellow">Entry</option>
+              <option id="orange">Junior</option>
+              <option id="red">Mid-Level</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group as={Col} controlId="formInputTimer">
+            <Form.Label>Time to Answer</Form.Label>
+            <Form.Control as="select">
+              <option>1 minute</option>
+              <option>2 minute</option>
+              <option>3 minute</option>
+              <option>4 minute</option>
+              <option>5 minute</option>
+            </Form.Control>
+          </Form.Group>
+        </Form.Row>
+        <Form.Row id="place">
+          <Form.Group as={Col} controlId="formInputQuestion">
+            <Form.Label>Add the Question You want to add.</Form.Label>
+            <Form.Control as="textarea" rows="6" />
+          </Form.Group>
+          <Form.Group as={Col} controlId="formInputAnswer">
+            <Form.Label>Add the Answer to the Question.</Form.Label>
+            <Form.Control as="textarea" rows="6" />
+          </Form.Group>
+          <Form.Group as={Col} controlId="formInputCode">
+            <Form.Label>If your Question has any code, Enter it here.</Form.Label>
+            <Form.Control placeholder="(Seperate it by Enter Key)" as="textarea" rows="6" />
+          </Form.Group>
+        </Form.Row>
+        {/* <Form.Group id="formGridCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group> */}
+        <Button id="btn" variant="primary" type="submit">
+          Submit
           </Button>
-            <Button variant="primary" type="submit">
-              Return to Dashboard
-          </Button>
-          </Form>
-        </Card.Body>
-      </Card>
-
-    </React.Fragment >
+      </Form>
+    </React.Fragment>
   );
 };
-
 export default Add;
