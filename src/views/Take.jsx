41f462699f0e6eff203f3bd6f91832100e9a266c
bcwahoo <bcwahoo@gmail.com>
2020-01-28 18:27:32 -0500

    Before Class

--- src/views/Take.jsx
+++ src/views/Take.jsx
@@ -1,19 +1,48 @@
 import React from "react";
+import { Form, Col, Row, Spinner } from "react-bootstrap";
 import { BrowserRouter as Route, Link } from "react-router-dom";
-import Home from "./Home";
+import { QuestionCard } from "../components";
 
 const Take = () => {
   return (
     <React.Fragment>
-      <div>Take mock interview</div>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Done - back to Dashboard</Link>
-          </li>
-        </ul>
-        <Route exact path="/" component={Home} />
-      </div>
+      <h2 className="text-center">Prep for Your Interview</h2>
+
+      <Form>
+        <Form.Row className="justify-content-center">
+          <Form.Group as={Col} xs={4} md={4} controlId="formInputLevel">
+            <Form.Label>Select Question Difficulty</Form.Label>
+            <Form.Control as="select">
+              <option>Entry</option>
+              <option>Junior</option>
+              <option>Mid-Level</option>
+            </Form.Control>
+          </Form.Group>
+        </Form.Row>
+      </Form>
+
+      <hr />
+
+      {/* THIS IS WHERE THE GAME QUESTIONS SHOULD LOAD UP */}
+
+      <Row className="py-1">
+        <Col xs={6} md={4}><QuestionCard /></Col>
+        <Col xs={6} md={4}><QuestionCard /></Col>
+        <Col xs={6} md={4}><QuestionCard /></Col>
+      </Row>
+
+      <Row className="py-1">
+        <Col xs={6} md={4}><QuestionCard /></Col>
+        <Col xs={6} md={4}><QuestionCard /></Col>
+        <Col xs={6} md={4}><QuestionCard /></Col>
+      </Row>
+
+      <Row className="py-1">
+        <Col xs={6} md={4}><QuestionCard /></Col>
+        <Col xs={6} md={4}><QuestionCard /></Col>
+        <Col xs={6} md={4}><QuestionCard /></Col>
+      </Row>
+
     </React.Fragment>
   );
 };
